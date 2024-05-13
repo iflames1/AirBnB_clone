4d68591f944a004dae1e013da13a6c4977e74462
Flames <isaacomenuche@gmail.com>
2024-05-13 23:31:22 +0100

    make genrate.sh dynamic

--- generate-authors.sh
+++ generate-authors.sh
@@ -2,16 +2,17 @@
 
 set -e
 
+
+# Get the absolute path of the directory containing this script
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Get the absolute path of the root directory of the repository
-ROOTDIR="/home/flames/alx/AirBnB_clone"    # Could be dynamic :(
+ROOTDIR="$(git -C "$SCRIPTDIR" rev-parse --show-toplevel)"
 
 set -x
 
-# Navigate to the root directory of the repository
-cd "$ROOTDIR" || exit
-
 # Generate the AUTHORS file in the root directory of the repository
-git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf > AUTHORS
+git -C "$ROOTDIR" log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf > "${ROOTDIR}/AUTHORS"
 
 # Display a message indicating that the AUTHORS file has been generated
 echo "AUTHORS file generated successfully at ${ROOTDIR}/AUTHORS"
