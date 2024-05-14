3392af382a76cad03085636f872912958c4b3152
Flames <isaacomenuche@gmail.com>
2024-05-14 09:08:36 +0100

    Add first base test

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
     def __init__(self) -> None:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self) -> str:
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,8 +2,45 @@
 """"""
 
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_id_generation(self):
+        self.assertIsNotNone(self.base_model.id)
+
+    def test_id_type(self):
+        self.assertIsInstance(self.base_model.id, str)
+
+    def test_created_at_and_updated_at(self):
+        now = datetime.now()
+        self.assertLessEqual(self.base_model.created_at, now)
+        self.assertLessEqual(self.base_model.updated_at, now)
+        self.assertEqual(self.base_model.created_at, self.base_model.updated_at)
+
+    def test_save_method(self):
+        previous_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(previous_updated_at, self.base_model.updated_at)
+        self.assertLessEqual(self.base_model.created_at, self.base_model.updated_at)
+
+    def test_to_dict_method(self):
+        obj_dict = self.base_model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict["__class__"], "BaseModel")
+        self.assertEqual(obj_dict["id"], self.base_model.id)
+        self.assertEqual(obj_dict["created_at"], self.base_model.created_at.isoformat())
+        self.assertEqual(obj_dict["updated_at"], self.base_model.updated_at.isoformat())
+
+    def test_str_method(self):
+        obj_str = str(self.base_model)
+        expected_str = f"[BaseModel] ({self.base_model.id}) {self.base_model.__dict__}"
+        self.assertEqual(obj_str, expected_str)
+
+
+if __name__ == "__main__":
+    unittest.main()
