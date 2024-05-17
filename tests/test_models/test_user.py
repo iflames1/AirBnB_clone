f9ef81fa65583445167d24d9227ea1b7ffbab8bb
Flames <isaacomenuche@gmail.com>
2024-05-17 11:55:22 +0100

    add test_user.py

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import unittest
+import json
+from models.user import User
+from models.engine.file_storage import FileStorage
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user = User()
+        self.storage = FileStorage()
+        self.user.email = "test@example.com"
+        self.user.password = "password123"
+        self.user.first_name = "Isaac"
+        self.user.last_name = "Flames"
+
+    def tearDown(self):
+        self.storage._FileStorage__object ={}
+
+    def test_attributes(self):
+        self.assertEqual(self.user.email, "test@example.com")
+        self.assertEqual(self.user.password, "password123")
+        self.assertEqual(self.user.first_name, "Isaac")
+        self.assertEqual(self.user.last_name, "Flames")
+        self.assertIsNotNone(self.user.created_at)
+        self.assertIsNotNone(self.user.updated_at)
+        self.assertIsNotNone(self.user.id)
+
+if __name__ == "__main__":
+    unittest.main()
