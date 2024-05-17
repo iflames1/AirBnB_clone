b7817480c1ca26d2c0ace06c4a25a20dffb52ec1
Flames <isaacomenuche@gmail.com>
2024-05-17 06:37:01 +0100

    add do_show method

--- console.py
+++ console.py
@@ -2,26 +2,44 @@
 
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    CLASSES = {"BaseModel": BaseModel}
 
 
     def do_create(self, arg):
         if not arg:
             print("** class doesn't exist **")
             return
-
-        classes = {"BaseModel": BaseModel}
-
         class_name = arg.split()[0]
-        if class_name in classes:
-            new_instance = classes[class_name]()
+        if class_name in self.CLASSES:
+            new_instance = self.CLASSES[class_name]()
             new_instance.save()
             print(new_instance.id)
         else:
             print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in self.CLASSES:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        obj_dict = storage.all()
+        if key in obj_dict:
+            print(obj_dict[key])
+        else:
+            print("** no instance found **")
+
 
     def do_quit(self, arg):
         """Quit command to exit the program \n"""
