c14323d70d0d1a9b180f8b8b80ec64a41e31fff6
Flames <isaacomenuche@gmail.com>
2024-05-17 06:11:09 +0100

    add create method

--- console.py
+++ console.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+
+
+    def do_create(self, arg):
+        if not arg:
+            print("** class doesn't exist **")
+            return
+
+        classes = {"BaseModel": BaseModel}
+
+        class_name = arg.split()[0]
+        if class_name in classes:
+            new_instance = classes[class_name]()
+            new_instance.save()
+            print(new_instance.id)
+        else:
+            print("** class doesn't exist **")
 
     def do_quit(self, arg):
         """Quit command to exit the program \n"""
@@ -14,8 +31,8 @@ class HBNBCommand(cmd.Cmd):
         """To exit the program using EOF \n"""
         return True
 
-    def emptyline(self):
-        """Called when an empty line is entered"""
+    def emptyine(self):
+        """Do nothing on empty line"""
         pass
 
 
