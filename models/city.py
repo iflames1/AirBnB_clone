79c30f8b641d5f77bf53387bea82c5c07b06b1bc
Flames <isaacomenuche@gmail.com>
2024-05-18 11:38:59 +0100

    docs city

--- models/city.py
+++ models/city.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
+"""
+Represents a city.
+"""
 
 from models.base_model import BaseModel
-from models.state import State
+
 
 class City(BaseModel):
+    """
+    Represents a city.
+
+    Attributes:
+        state_id (str): The ID of the state the city belongs to.
+        name (str): The name of the city.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the class with optional arguments
+        provided in kwargs.
+
+        Parameters:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            None
+        """
         super().__init__(*args, **kwargs)
-        self.state_id = "" #State.id
+        self.state_id = ""
         self.name = ""
