9b6f77cf9d7811c968acd9a1dbd947129456fe91
Flames <isaacomenuche@gmail.com>
2024-05-18 16:41:26 +0100

    test state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import unittest
+from models.state import State
+from models.engine.file_storage import FileStorage
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.state = State()
+        self.storage = FileStorage()
+        self.state.name = "Flames"
+
+
+
+    def tearDown(self):
+        self.storage._FileStorage__object ={}
+
+    def test_attributes(self):
+        self.assertEqual(self.state.name, "Flames")
+        self.assertIsNotNone(self.state.created_at)
+        self.assertIsNotNone(self.state.updated_at)
+        self.assertIsNotNone(self.state.id)
+
+if __name__ == "__main__":
+    unittest.main()
