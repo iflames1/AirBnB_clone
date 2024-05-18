95f6cfc0b161c7cf9c4731d53b18ab5b28e3165b
Flames <isaacomenuche@gmail.com>
2024-05-18 11:56:14 +0100

    doc user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
+"""
+Represents a user in the application.
+"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """
+    Represents a user in the application.
+
+    The `User` class inherits from the `BaseModel` class and provides
+    properties for storing a user's email, password, first name, and last name.
+
+    Attributes:
+        email (str): The user's email address.
+        password (str): The user's password.
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the User class with optional arguments
+        provided in kwargs.
+
+        Parameters:
+            *args (tuple): Variable length argument list.
+            **kwargs (dict): Arbitrary keyword arguments.
+
+        Returns:
+            None
+        """
         super().__init__(*args, **kwargs)
         self.email = ""
         self.password = ""
