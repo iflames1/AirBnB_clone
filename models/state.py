b5c7e401ea9f0dbdfbb864dbb8b6cc8b25f6e4d3
Flames <isaacomenuche@gmail.com>
2024-05-18 11:54:11 +0100

    document state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
+"""
+Represents a state in the application.
+"""
 
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
+    """
+    Represents a state in the application.
+
+    Attributes:
+        name (str): The name of the state.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the class with optional arguments
+        provided in kwargs.
+
+        Parameters:
+            *args (tuple): Variable length argument list.
+            **kwargs (dict): Arbitrary keyword arguments.
+
+        Returns:
+            None
+        """
         super().__init__(*args, **kwargs)
         self.name = ""
