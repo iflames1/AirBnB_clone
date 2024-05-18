56e55f9aa4ab39ef750a3533f8345c7763fd04b1
Flames <isaacomenuche@gmail.com>
2024-05-18 11:26:55 +0100

    Document amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
+"""
+Initializes a new instance of the class with optional arguments provided in
+kwargs.
+"""
 
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """
+    Represents an amenity in the application.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the class with optional arguments
+        provided in kwargs.
+
+        Parameters:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            None
+        """
         super().__init__(*args, **kwargs)
         self.name = ""
